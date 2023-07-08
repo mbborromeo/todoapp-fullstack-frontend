f6667286a76dbd13ad6a9cf20edd8ccb73606691
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-07-08 11:38:51 -0700

    import Material-UI components

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,45 @@
 import "./App.css";
 
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
-
-/* React routes */
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Root element</div>,
-  },
-  {
-    path: "/add",
-    element: <div>Add component</div>,
-  },
-  {
-    path: "/deleteall",
-    element: <div>Delete All component</div>,
-  },
-  {
-    path: "/search",
-    element: <div>Search component</div>,
-  },
-]);
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <header>
+        <h1>Marvelous v2.0</h1>
+        <a href="#">Delete all tasks</a>
+      </header>
+      <br />
+      <br />
+
+      <TextField id="outlined-basic" variant="outlined" />
+
+      <Button variant="contained">Add</Button>
+      <br />
+
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        placeholder="Search..."
+      />
+
+      <h3>To Do</h3>
+      <hr />
+      <FormGroup>
+        <FormControlLabel control={<Checkbox />} label="Task 1" />
+        <FormControlLabel control={<Checkbox />} label="Task 2" />
+      </FormGroup>
+
+      <h3>Done</h3>
+      <hr />
+      <FormGroup>
+        <FormControlLabel control={<Checkbox checked />} label="Task 3" />
+        <FormControlLabel control={<Checkbox checked />} label="Task 4" />
+      </FormGroup>
     </>
   );
 }
