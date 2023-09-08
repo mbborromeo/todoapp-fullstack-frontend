e83f73ef1ac7b5fad677c7f97afdd38174346465
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-09-08 10:47:29 -0700

    edit Eslint rule comma-dangle warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     "airbnb",
     "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
+    "plugin:react-hooks/recommended"
   ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: "script"
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   rules: {
     "prettier/prettier": ["error", { endOfLine: "auto" }],
     "linebreak-style": "off",
     "no-underscore-dangle": "off",
+    "comma-dangle": "warn",
     quotes: "off",
     "react/jsx-filename-extension": [
       1,
       {
-        extensions: [".js", ".jsx"],
-      },
+        extensions: [".js", ".jsx"]
+      }
     ],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-  },
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
